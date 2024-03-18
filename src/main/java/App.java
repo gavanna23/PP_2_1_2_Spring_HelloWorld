05a4ae388e3d2b46d4c98ed52e294a53d8084c2f
Anna <gavanna09@yandex.ru>
2024-03-18 11:32:29 +0400

    new

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,21 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        System.out.println(bean == bean2);
+
+
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat.getName());
+
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat2.getName());
+
+        System.out.println(cat == cat2);
+
     }
 }
